1fcfdf963701242b6a2f75168e3f6b7942bf1c5d
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-06-11 15:27:33 +0530

    Add files via upload

---
+++ WhiteSpaceAndIndentation.java
@@ -0,0 +1,28 @@
+package StatementsExpressionsCodeBlocksAndMethods;
+
+public class WhiteSpaceAndIndentation {
+
+	    public static void main(String[] args) {
+
+	        int myVariable = 50;
+
+	        if (myVariable == 50) {
+	            System.out.println("Printed");
+	        }
+
+
+	        myVariable++;
+	        myVariable--;
+	        System.out.println("This is a test");
+
+	        System.out.println("This is" +
+	                " another" +
+	                " still more.");
+
+	        int anotherVariable = 50;
+	        myVariable--;
+	        System.out.println("This is another one");
+	    }
+	}
+
+

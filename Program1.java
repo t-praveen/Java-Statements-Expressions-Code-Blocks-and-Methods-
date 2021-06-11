3a0b53d309c9fac480f01c74df01eab2125a0f34
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-06-11 14:55:52 +0530

    Add files via upload

---
+++ Program1.java
@@ -0,0 +1,17 @@
+package StatementsExpressionsCodeBlocksAndMethods;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+	
+		   // a mile is equal to 1.609344 kilometers
+		    double kilometers = (100 * 1.609344); // 100 * 1.609344 is an expression
+		    int highScore = 50; // whole line starting from data type to semicolon is a statement 
+		    System.out.println("Total kilometers is : "+kilometers);
+		    if(highScore == 50) 
+		    {
+		        System.out.println("This is an expression");
+
+		    }
+	}
+}
